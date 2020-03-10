53eac4aa667026602dff5d92986fff1074aa1175
brianineza01 <60552218+brianineza01@users.noreply.github.com>
2020-03-10 23:35:25 +0200

    pushing changes

--- server/config/dbconnect.js
+++ server/config/dbconnect.js
@@ -2,6 +2,11 @@ import { Client } from 'pg';
 import { config } from 'dotenv';
 config();
 
-const client = new Client(process.env.DATABASE_URL);
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+      rejectUnauthorized: false
+  } 
+});
 console.log(client.query)
 export default client;
